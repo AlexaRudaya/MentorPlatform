314038fe57f15c0afbb285a0c22c64530e9f4f7d
Alexandra Rudaya <alexa.rudaya@gmail.com>
2023-08-15 09:39:31 +0300

    MentorshipSubjectValidator is added.

---
+++ Mentors.ApplicationCore/Validators/MentorshipSubjectValidator.cs
@@ -0,0 +1,14 @@
+﻿namespace Mentors.ApplicationCore.Validators
+{
+    public sealed class MentorshipSubjectValidator : AbstractValidator<MentorshipSubjectDto>
+    {
+        public MentorshipSubjectValidator()
+        {
+            RuleFor(subjectDto => subjectDto.Name)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("The name must be set")
+                .Length(10, 300);
+        }
+    }
+}
