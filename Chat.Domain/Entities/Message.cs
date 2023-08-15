7b417a648db6d0e55e03c7d85afbbe90816897ba
Alexandra Rudaya <alexa.rudaya@gmail.com>
2023-08-15 14:41:04 +0300

    Domain entities are added.

---
+++ Chat.Domain/Entities/Message.cs
@@ -0,0 +1,11 @@
+﻿namespace Chat.Domain.Entities
+{
+    public class Message : BaseEntity
+    {
+        public string Content { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public User User { get; set; }
+    }
+}
