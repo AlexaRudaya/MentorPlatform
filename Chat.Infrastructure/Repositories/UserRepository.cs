05f9e36403f496748d13da557f8f3c5fee76a8c4
Alexandra Rudaya <alexa.rudaya@gmail.com>
2023-08-18 12:27:37 +0300

    BaseRepository + UserRepository are created.

---
+++ Chat.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,13 @@
+﻿using Chat.Domain.Entities;
+using Chat.Domain.IRepository;
+using Chat.Infrastructure.Data;
+
+namespace Chat.Infrastructure.Repositories
+{
+    public class UserRepository : BaseRepository<User>, IUserRepository
+    {
+        public UserRepository(ChatDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
