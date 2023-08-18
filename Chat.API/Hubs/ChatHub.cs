613c14f175583bd03e75c19689f476fe81b2ad31
Alexandra Rudaya <alexa.rudaya@gmail.com>
2023-08-18 11:10:26 +0300

    Logic for leaving the chat and connection closing  is implemented.

--- Chat.API/Hubs/ChatHub.cs
+++ Chat.API/Hubs/ChatHub.cs
@@ -9,6 +9,7 @@ namespace Chat.API.Hubs
     {
         private readonly ChatDbContext _context;
         private readonly ILogger<ChatHub> _logger;
+        private static readonly Dictionary<string, string> _connectedUsers = new();
 
         public ChatHub(
             ChatDbContext context,
@@ -50,13 +51,35 @@ namespace Chat.API.Hubs
             var message = new Message
             {
                 Content = content,
-                UserId = user.Id 
+                UserId = user.Id
             };
 
             _context.Messages.Add(message);
             await _context.SaveChangesAsync();
 
+            _connectedUsers[Context.ConnectionId] = userName;
+
             await Clients.Others.SendAsync("ReceiveMessage", userName, content);
+        }
+
+        public async Task LeaveChat()
+        {
+            if (_connectedUsers.TryGetValue(Context.ConnectionId, out string userName))
+            {
+                var message = $"{userName} left the chat";
+                await Clients.Others.SendAsync("ReceiveMessage", userName, message);
+
+                _logger.LogInformation($"User {userName} is leaving the chat.");
+
+                _connectedUsers.Remove(Context.ConnectionId);
+            }
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            await LeaveChat();
+            await base.OnDisconnectedAsync(exception);
+
         }
     }
 }
