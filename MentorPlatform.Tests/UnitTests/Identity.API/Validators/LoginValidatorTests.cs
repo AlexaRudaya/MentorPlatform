f30925c429325edf9919999a271e0980ffc547e9
Alexandra Rudaya <alexa.rudaya@gmail.com>
2023-08-30 13:04:54 +0300

    Tests for the Login Validor are added.

---
+++ MentorPlatform.Tests/UnitTests/Identity.API/Validators/LoginValidatorTests.cs
@@ -0,0 +1,145 @@
+﻿using FluentValidation.TestHelper;
+using Identity.ApplicationCore.Validator;
+using MentorPlatform.Tests.UnitTests.Identity.API.BogusData;
+
+namespace MentorPlatform.Tests.UnitTests.Identity.API.Validators
+{
+    public class LoginValidatorTests
+    {
+        private readonly LoginValidator _loginValidator;
+        private readonly LoginDataGenerator _loginData;
+
+        public LoginValidatorTests()
+        {
+            _loginValidator = new LoginValidator();
+            _loginData = new LoginDataGenerator();
+        }
+
+        [Fact]
+        public async Task ValidateLoginDto_ShouldBeSuccessfulValidation()
+        {
+            // Arrange
+            var loginDto = _loginData.GenerateFakeData();
+
+            // Act
+            var result = await _loginValidator.TestValidateAsync(loginDto);
+
+            // Assert
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+        // Testing Email
+        [Fact]
+        public async Task ValidateLoginDto_When_Email_IsEmpty_ShouldFailValidation()
+        {
+            // Arrange
+            var loginDto = _loginData.GenerateFakeData();
+            loginDto.Email = "";
+
+            // Act
+            var result = await _loginValidator.TestValidateAsync(loginDto);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(loginDto => loginDto.Email);
+        }
+
+        [Fact]
+        public async Task ValidateLoginDto_When_Email_IsInvalid_ShouldFailValidation()
+        {
+            // Arrange
+            var loginDto = _loginData.GenerateFakeData();
+            loginDto.Email = "emailWithoutAddressSign";
+
+            // Act
+            var result = await _loginValidator.TestValidateAsync(loginDto);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(loginDto => loginDto.Email);
+        }
+
+        // Testing Password
+        [Fact]
+        public async Task ValidateLoginDto_When_Password_IsEmpty_ShouldFailValidation()
+        {
+            // Arrange
+            var loginDto = _loginData.GenerateFakeData();
+            loginDto.Password = "";
+
+            // Act
+            var result = await _loginValidator.TestValidateAsync(loginDto);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(loginDto => loginDto.Password);
+        }
+
+        [Fact]
+        public async Task ValidateLoginDto_When_Password_IsShort_ShouldFailValidation()
+        {
+            // Arrange
+            var loginDto = _loginData.GenerateFakeData();
+            loginDto.Password = "abc";
+
+            // Act
+            var result = await _loginValidator.TestValidateAsync(loginDto);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(loginDto => loginDto.Password);
+        }
+
+        [Fact]
+        public async Task ValidateLoginDto_When_Password_HasNoSpecialChars__ShouldFailValidation()
+        {
+            // Arrange
+            var loginDto = _loginData.GenerateFakeData();
+            loginDto.Password = "aBcdf127";
+
+            // Act
+            var result = await _loginValidator.TestValidateAsync(loginDto);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(loginDto => loginDto.Password);
+        }
+
+        [Fact]
+        public async Task ValidateLoginDto_When_Password_HasNoNumber__ShouldFailValidation()
+        {
+            // Arrange
+            var loginDto = _loginData.GenerateFakeData();
+            loginDto.Password = "aBcdfKl!";
+
+            // Act
+            var result = await _loginValidator.TestValidateAsync(loginDto);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(loginDto => loginDto.Password);
+        }
+
+        [Fact]
+        public async Task ValidateLoginDto_When_Password_HasNoUppercase__ShouldFailValidation()
+        {
+            // Arrange
+            var loginDto = _loginData.GenerateFakeData();
+            loginDto.Password = "abcdfk2l!";
+
+            // Act
+            var result = await _loginValidator.TestValidateAsync(loginDto);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(loginDto => loginDto.Password);
+        }
+
+        [Fact]
+        public async Task ValidateLoginDto_When_Password_HasNoLowercase__ShouldFailValidation()
+        {
+            // Arrange
+            var loginDto = _loginData.GenerateFakeData();
+            loginDto.Password = "ABCDFG9!";
+
+            // Act
+            var result = await _loginValidator.TestValidateAsync(loginDto);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(loginDto => loginDto.Password);
+        }
+    }
+}
